0ae6886b3745424a769aa3f1fbbcb5d18bddbe3e
fokuoisaac <gedysqnysonic@gmail.com>
2025-07-19 09:30:32 +0000

    working on the hosting so everyone can access

--- voting-backend/server.js
+++ voting-backend/server.js
@@ -25,6 +25,7 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`🚀 Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`🚀 Server running on http://0.0.0.0:${PORT}`);
 });
+
